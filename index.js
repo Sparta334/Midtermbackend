a0d6ddfd730489308d0272a3e429676adbdaf255
Spartan-117 <ianmina0322@gmail.com>
2023-04-07 20:42:12 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 8000;
+var recombee = require('recombee-api-client');
+var rqs = recombee.requests;
+
+var client = new recombee.ApiClient(
+  'exodia-the-forbidden-one-dev', 
+  'bx6IIFrZ7tzVnBplVVGhaQCdUKLVHWcELOMsUWH1orFxefDJtPuOzIFGT8Ck7Gdn', 
+  { region: 'ap-se' }
+);
+
+function findObjectByPropertyValue(jsonArray, propertyName, targetValue) {
+    for (let i = 0; i < jsonArray.length; i++) {
+      if (jsonArray[i][propertyName] === targetValue) {
+        return jsonArray[i];
+      }
+    }
+    return null;
+  }
+
+
+app.use(cors());
+
+app.get('/BackEnd/SearchContent/:value' , (req ,res) =>{
+
+    const value = req.params.value;
+    
+     client.send(new rqs.ListItems({
+        // optional parameters:
+        'returnProperties': true
+      }) , (err, response) =>{
+        
+        const filteredProducts = response.filter((product) => {
+            return product.ProductName.toLowerCase().includes(value.toLowerCase());
+        });
+
+        const limitedProducts = filteredProducts.slice(0, 5).map((product) => {
+            return { product };
+          });
+
+  
+        console.log(limitedProducts)
+        res.send(limitedProducts)
+
+      } )
+
+      
+     
+     
+});
+
+
+app.get('/BackEnd/Products/:value', (req ,res) => {
+
+
+    const value = decodeURIComponent(req.params.value);
+
+
+    client.send(new rqs.ListItems({
+        'returnProperties': true
+    }) , (err, response) =>{
+
+        const result = findObjectByPropertyValue(response, 'ProductName', value);
+        console.log(result)
+        res.send(result);
+     }
+    
+    
+    )
+
+  
+    });
+    
+
+app.listen(port , () =>{
+
+    console.log("A")
+
+})
