56151047bd38a162809d6a9cabeeaf08cae6ce65
Spartan-117 <ianmina0322@gmail.com>
2023-04-09 00:24:41 +0800

    QQ

--- index.js
+++ index.js
@@ -4,9 +4,27 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 8000;
 var recombee = require('recombee-api-client');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+require('dotenv').config();
 var rqs = recombee.requests;
+const session = require('express-session');
+const request = require('request');
+const passport = require('passport');
+var cookieParser = require('cookie-parser');
+const GitHubStrategy = require('passport-github').Strategy;
+
+var RedisStore = require('connect-redis');
+var redis = require("redis").createClient();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 var client = new recombee.ApiClient(
+
+
   'exodia-the-forbidden-one-dev', 
   'bx6IIFrZ7tzVnBplVVGhaQCdUKLVHWcELOMsUWH1orFxefDJtPuOzIFGT8Ck7Gdn', 
   { region: 'ap-se' }
@@ -21,8 +39,14 @@ function findObjectByPropertyValue(jsonArray, propertyName, targetValue) {
     return null;
   }
 
+  app.use(session({
+    secret: 'your-secret-key',
+    resave: false, // or true
+    saveUninitialized: false // or true
+  }));
 
-app.use(cors());
+
+  app.use(cors());
 
 app.get('/BackEnd/SearchContent/:value' , (req ,res) =>{
 
@@ -53,6 +77,11 @@ app.get('/BackEnd/SearchContent/:value' , (req ,res) =>{
 });
 
 
+
+
+
+
+
 app.get('/BackEnd/Products/:value', (req ,res) => {
 
 
@@ -75,8 +104,87 @@ app.get('/BackEnd/Products/:value', (req ,res) => {
     });
     
 
-app.listen(port , () =>{
 
-    console.log("A")
+
+//登入
+
+/**
+ * -----------------------------------------------------------------------------
+ * Setup
+ * -----------------------------------------------------------------------------
+ */
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+// 将用户 ID 序列化到 session 中
+passport.serializeUser((user, done) => {
+done(null, user.id);
+});
+
+// 从 session 中反序列化用户 ID
+passport.deserializeUser((id, done) => {
+done(null, { id });
+});
+
+
+app.get('/BackEnd/Auth/:Provider' ,  (req , res)  => {
+
+
+
+    const Provider =  req.params.Provider;
+
+
+} );
+
+
+passport.use(new GitHubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:4000/auth/github/callback"
+},
+    function(accessToken, refreshToken, profile, cb) {
+  // 在此处处理用户信息
+  return cb(null, profile);
+  }
+));
+
+
+
+
+app.get('/logout', (req, res) => {
+  req.logout();
+});
+
+
+app.get('/auth/github',  passport.authenticate('github'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { successRedirect: '/profile',  failureRedirect: '/pages/login' }));
+
+
+app.get('/profile'), ( (req, res) => {
+  // 检查用户是否已登录
+
+  res.send(req.user.id);
 
 })
+
+
+app.listen(port , () =>{
+
+  console.log("A")
+
+})
+
