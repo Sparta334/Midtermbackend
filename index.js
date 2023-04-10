7b87f80378f032acb70d5d5797bb7ef9e7caeb0d
Spartan-117 <ianmina0322@gmail.com>
2023-04-10 22:15:11 +0800

    ww

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ app.post('/BackEnd/Detail',( req, res, next) => {
   const userData = req.data.UserData;
   const userViewData = req.data.UserViewData
 
-  console.log(req.data.userData)
+  console.log(userData)
 
   client.send(new rqs.AddDetailView(userData, userViewData, {timestamp: '2014-07-20T02:49:45+02:00', cascadeCreate: true}),
     (err, response) => {
