942d520d72551c023f53225af77b5e6e7ff69601
Spartan334 <ianmina0322@gmail.com>
2023-05-27 23:52:05 +0800

    222

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ app.post('/BackEnd/Profile',( req, res) => {
   const firstItem = UserItem[0];
   const itemId = firstItem.itemId;
 
-  client.send(new rqs.SearchItems("undefined", itemId , 1, {
+  client.send(new rqs.SearchItems("undefined", JSON.stringify(itemId) , 1, {
     'cascadeCreate': false,
     'returnProperties': true,
   }), (err, response) =>{
