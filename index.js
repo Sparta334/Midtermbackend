8ac842630a41c56922558eeae2e70a01e2001105
Spartan334 <ianmina0322@gmail.com>
2023-05-27 21:36:01 +0800

    eee

--- index.js
+++ index.js
@@ -232,10 +232,16 @@ app.post('/BackEnd/Profile',( req, res) => {
   const userData = req.body.data.UserData;
 
 
-  client.send(new rqs.AddUser(JSON.stringify(userData)));
-  console.log(userData)
+  client.send(new rqs.AddUser(userData),
   
-  })
+  (err, response) =>{
+    
+    console.log(response)
+    
+  });
+  
+  
+ })
 
 
 
