ee6196b7b6c16a199fa75ff699aecf330a2b80a4
Spartan334 <ianmina0322@gmail.com>
2023-05-29 15:47:18 +0800

    ss

--- index.js
+++ index.js
@@ -189,13 +189,14 @@ app.post('/BackEnd/Profile',( req, res) => {
       console.log("limitedProducts : " +JSON.stringify(UserItem));
       
       console.log("Result : " + UserItem.itemId);
-      
+      const itemID = UserItem.itemId;
        
-    client.send(new rqs.SearchItems(userData, UserItem.itemId, 1, {
-      'cascadeCreate': false,
-      'includedProperties': ['itemId'],
+    client.send(new rqs.ListItems({
+      'filter': "string(itemID)' == context_item[\"itemId\"]",
+      'count': 1,
       'returnProperties': true,
-    }), (err, responses) =>{
+    
+    }) ), (err, responses) =>{
   
         console.log(responses)
         res.send(responses);
